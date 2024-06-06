3a792db59aec8755f979362c93ae59817d774e5e
Dexter <dex-hon-sci@outlook.com>
2024-06-07 00:04:01 +0800

    finish fast download function.

--- update_db.py
+++ update_db.py
@@ -10,7 +10,7 @@ It pulls data from external servers to the local directory.
 
 """
 import datetime as datetime
-
+import pandas as pd
 import EC_tools.read as read
 import EC_tools.utility as util
 
@@ -21,14 +21,14 @@ __author__="Dexter S.-H. Hon"
 #             'password':"76tileArg56!"}
 # =============================================================================
 
-auth_pack = {'username': "leigh@eulercapital.com.au",
+AUTH_PACK = {'username': "leigh@eulercapital.com.au",
             'password':"Li.96558356"}
 
-date_pack = {"start_date": "2021-01-01",
+DATE_PACK = {"start_date": "2021-01-01",
         "end_date": "2024-05-18"}
 
 # =============================================================================
-asset_pack = {'categories': 'Argus Nymex WTI month 1, Daily',
+ASSET_PACK = {'categories': 'Argus Nymex WTI month 1, Daily',
                'keywords': "WTI",
                'symbol': "CL"}
 # =============================================================================
@@ -43,10 +43,11 @@ categories_monthly_list = [  'Argus Nymex WTI front month average, Monthly',
                                  'Nymex RBOB gasoline front month average, Monthly',
                                  'Argus Brent front month average, Monthly',
                                  'ICE gasoil front month average, Monthly']
+
 # checking function to see if the table is up to date
 
 
-def download_latest_APC(auth_pack, asset_pack):
+def download_latest_APC(auth_pack, asset_pack, start_date = "2021-01-01"):
     # input is a dictionary or json file
     username = auth_pack['username']
     password = auth_pack['password']
@@ -54,30 +55,42 @@ def download_latest_APC(auth_pack, asset_pack):
     categories = asset_pack['categories']
     keywords = asset_pack['keywords']
     symbol = asset_pack['symbol']
-    
-    start_date = "2021-01-01"
     end_date = datetime.date.today().strftime("%Y-%m-%d")
-        
+    
     # download the relevant APC data from the server
     signal_data = read.get_apc_from_server(username, password, start_date, 
                                       end_date, categories,
                             keywords=keywords, symbol=symbol)
     return signal_data
 
-def fast_download_latest_APC(auth_pack, asset_pack, old_filename): #WIP
-    # input is a dictionary or json file
-    username = auth_pack['username']
-    password = auth_pack['password']
+def download_latest_APC_fast(auth_pack, asset_pack, old_filename): #tested
     
-    categories = asset_pack['categories']
-    keywords = asset_pack['keywords']
-    symbol = asset_pack['symbol']
+    # Check the last entry in the old file and only download the data 
+    # Read the old files
+    old_data = pd.read_csv(old_filename)
     
-    #check the last entry in the old file and only download the data 
+    #Find the date of the latest entry
+    latest_entry = str(old_data['Forecast Period'].iloc[-1])
+    
+    
+    # download the latest APC from the latest_entry till today
+    temp = download_latest_APC(auth_pack, asset_pack, start_date = latest_entry)
+    
+    # for some reason I have to turn the Forecast column elements to str first 
+    # to align them with the old data
+    temp['Forecast Period'] = [temp['Forecast Period'].iloc[i].\
+                                      strftime("%Y-%m-%d") for 
+                                i, _ in enumerate(temp['Forecast Period'])]
+    
+    # concandenate the old file
+    signal_data = pd.concat([old_data, temp], ignore_index = True)
+    signal_data.sort_values(by='Forecast Period')
+    
+    return signal_data
 
 @util.time_it
 def download_latest_APC_list(auth_pack, save_filename_list, categories_list, 
-                         keywords_list, symbol_list):
+                         keywords_list, symbol_list, fast_dl=True):
     # a function to download the APC of a list of asset
     # input username and password.json
 
@@ -86,7 +99,13 @@ def download_latest_APC_list(auth_pack, save_filename_list, categories_list,
         @util.save_csv("{}".format(filename))
         def download_latest_APC_indi(cat, key, sym):
             asset_pack = {'categories': cat, 'keywords': key, 'symbol': sym}
-            signal_data = download_latest_APC(auth_pack, asset_pack)
+            
+            if fast_dl == True: # Fast download. It loads old files
+                signal_data = download_latest_APC_fast(auth_pack, asset_pack, 
+                                                   filename)
+            elif fast_dl == False: # Slow download. It downlaod all data from db fresh
+                signal_data = download_latest_APC(auth_pack, asset_pack)
+                
             print("File: {} is generated.".format(filename))
 
             return signal_data
@@ -95,10 +114,28 @@ def download_latest_APC_list(auth_pack, save_filename_list, categories_list,
     
     return "All APC files downloaded!"
 
-
-def operate_list(auth_pack, save_filename_list, categories_list, 
-                         keywords_list, symbol_list):
-    return 
+# =============================================================================
+# @util.time_it
+# def download_latest_APC_fast_list(auth_pack, save_filename_list, categories_list, 
+#                          keywords_list, symbol_list): #tested
+#     # a function to download the APC of a list of asset
+#     # input username and password.json
+# 
+#     for filename, cat, key, sym in zip(save_filename_list, categories_list, 
+#                                        keywords_list, symbol_list):
+#         @util.save_csv("{}".format(filename))
+#         def download_latest_APC_fast_indi(cat, key, sym):
+#             asset_pack = {'categories': cat, 'keywords': key, 'symbol': sym}
+#             signal_data = download_latest_APC_fast(auth_pack, asset_pack, 
+#                                                    filename)
+#             print("File: {} is generated.".format(filename))
+# 
+#             return signal_data
+#         
+#         download_latest_APC_fast_indi(cat, key, sym)
+#     
+#     return "All APC files downloaded!"
+# =============================================================================
 
 def download_latest_Portara():
     # WIP
@@ -106,22 +143,27 @@ def download_latest_Portara():
     return None
 
 if __name__ == "__main__": 
-    save_filename_list = ["APC_latest_CLc2.csv", 
-                     "APC_latest_HOc2.csv", 
-                     "APC_latest_RBc2.csv", 
-                     "APC_latest_QOc2.csv",
-                     "APC_latest_QPc2.csv" ]
+    save_filename_list = ["/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/experiment/APC_latest_CLc1.csv", 
+                     "/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/experiment/APC_latest_HOc1.csv", 
+                     "/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/experiment/APC_latest_RBc1.csv", 
+                     "/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/experiment/APC_latest_QOc1.csv",
+                     "/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/experiment/APC_latest_QPc1.csv" ]
 
-    categories_list = ['Argus Nymex WTI month 2, Daily', 
-                   'Argus Nymex Heating oil month 2, Daily', 
-                   'Argus Nymex RBOB Gasoline month 2, Daily', 
-                   'Argus Brent month 2, Daily', 
-                   'Argus ICE gasoil month 2, Daily']
+    categories_list = ['Argus Nymex WTI month 1, Daily', 
+                   'Argus Nymex Heating oil month 1, Daily', 
+                   'Argus Nymex RBOB Gasoline month 1, Daily', 
+                   'Argus Brent month 1, Daily', 
+                   'Argus ICE gasoil month 1, Daily']
 
     keywords_list = ["WTI","Heating", "Gasoline",'Brent', "gasoil"]
-    symbol_list = ['CLc2', 'HOc2', 'RBc2', 'QOc2', 'QPc2']
+    symbol_list = ['CLc1', 'HOc1', 'RBc1', 'QOc1', 'QPc1']
     
-    download_latest_APC_list(auth_pack, save_filename_list, categories_list, 
-                             keywords_list, symbol_list)
+    #download_latest_APC_list(AUTH_PACK, save_filename_list, categories_list, 
+    #                         keywords_list, symbol_list)
     
+    download_latest_APC_list(AUTH_PACK, save_filename_list, categories_list, 
+                             keywords_list, symbol_list,fast_dl=True)    
     #download_latest_APC(auth_pack, asset_pack)
+
+    #download_latest_APC_fast(AUTH_PACK, ASSET_PACK, 
+    #            "/home/dexter/Euler_Capital_codes/EC_tools/data/APC_latest/dump/APC_latest_CLc1.csv")
