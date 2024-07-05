374df5e86c0373af340bd836d2869430296943de
Dexter <dex-hon-sci@outlook.com>
2024-07-06 03:58:17 +0800

    Strategy module start refactoring

--- EC_tools/strategy2.py
+++ EC_tools/strategy2.py
@@ -44,3 +44,19 @@ class MRStrategyArgus(object):
         self._buy_cond = False
         self._sell_cond = False
         self._neutral_cond = False
+    
+    def gen_strategy_data(history_data_lag5, apc_curve_lag5, curve_today,
+                          strategy_name="benchmark"):
+        pass
+    
+    def strategy_cond(self, data0, open_price, curve_today, apc_mid_Q = 0.5, 
+                                 apc_trade_Qrange=(0.4,0.6), 
+                                 apc_trade_Qlimit=(0.05,0.95)):
+        pass
+    
+    def set_EES(cond, curve_today, buy_range=(0.4,0.6,0.1), 
+                            sell_range =(0.6,0.4,0.9)):
+        pass
+    def apply_strategy():
+        pass
+    
