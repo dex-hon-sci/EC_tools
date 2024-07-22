77060b7e60714ee4e23df05256a50408793002ad
Dexter <dex-hon-sci@outlook.com>
2024-07-22 14:29:08 +0800

    cleanup the PNL plot script and add new functionality

--- run_PNL_plot.py
+++ run_PNL_plot.py
@@ -11,79 +11,18 @@ import numpy as np
 import matplotlib.dates as mdates
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
-from datetime import datetime
+import datetime as datetime
 
 #FILENAME = './data/profits_and_losses_data_benchmark_11_.xlsx'
 #FILENAME = '/home/dexter/Euler_Capital_codes/EC_tools/results/benchmark_PNL_xlsx/benchmark_PNL_full_.xlsx'
-FILENAME = '/home/dexter/Euler_Capital_codes/EC_tools/results/argus_exact_PNL_amb2_full_.xlsx'
+ARGUS_EXACT_PNL_FILENAME = '/home/dexter/Euler_Capital_codes/EC_tools/results/argus_exact_PNL_full_.xlsx'
+ARGUS_EXACT_PNL_AMB_FILENAME = '/home/dexter/Euler_Capital_codes/EC_tools/results/argus_exact_PNL_amb1_full_.xlsx'
+ARGUS_EXACT_PNL2_AMB_FILENAME = '/home/dexter/Euler_Capital_codes/EC_tools/results/argus_exact_PNL_amb2_full_.xlsx'
 
-dataframe = pd.read_excel(FILENAME,sheet_name='Total')
-
-dataframe_CLc1 = pd.read_excel(FILENAME, sheet_name='CLc1')
-dataframe_HOc1 = pd.read_excel(FILENAME, sheet_name='HOc1')
-dataframe_RBc1 = pd.read_excel(FILENAME, sheet_name='RBc1')
-dataframe_QOc1 = pd.read_excel(FILENAME, sheet_name='QOc1')
-dataframe_QPc1 = pd.read_excel(FILENAME, sheet_name='QPc1')
-
-dataframe_CLc2 = pd.read_excel(FILENAME, sheet_name='CLc2')
-dataframe_HOc2 = pd.read_excel(FILENAME, sheet_name='HOc2')
-dataframe_RBc2 = pd.read_excel(FILENAME, sheet_name='RBc2')
-dataframe_QOc2 = pd.read_excel(FILENAME, sheet_name='QOc2')
-dataframe_QPc2 = pd.read_excel(FILENAME, sheet_name='QPc2')
-
-date_match_col = 'Entry_Date'
-date_all = dataframe[date_match_col].to_numpy()
-cumPNL_all = dataframe['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-
-date_CLc1 = dataframe_CLc1[date_match_col].to_numpy()
-date_HOc1 = dataframe_HOc1[date_match_col].to_numpy()
-date_RBc1 = dataframe_RBc1[date_match_col].to_numpy()
-date_QOc1 = dataframe_QOc1[date_match_col].to_numpy()
-date_QPc1 = dataframe_QPc1[date_match_col].to_numpy()
-
-date_CLc2 = dataframe_CLc2[date_match_col].to_numpy()
-date_HOc2 = dataframe_HOc2[date_match_col].to_numpy()
-date_RBc2 = dataframe_RBc2[date_match_col].to_numpy()
-date_QOc2 = dataframe_QOc2[date_match_col].to_numpy()
-date_QPc2 = dataframe_QPc2[date_match_col].to_numpy()
-
-date_all = np.array([datetime.strptime(date_all[i], '%Y-%m-%d') 
-                      for i in range(len(date_all))])
-
-
-date_CLc1 = np.array([datetime.strptime(date_CLc1[i], '%Y-%m-%d') 
-                      for i in range(len(date_CLc1))])
-date_HOc1 = np.array([datetime.strptime(date_HOc1[i], '%Y-%m-%d') 
-                      for i in range(len(date_HOc1))])
-date_RBc1 = np.array([datetime.strptime(date_RBc1[i], '%Y-%m-%d') 
-                      for i in range(len(date_RBc1))])
-date_QOc1 = np.array([datetime.strptime(date_QOc1[i], '%Y-%m-%d') 
-                      for i in range(len(date_QOc1))])
-date_QPc1 = np.array([datetime.strptime(date_QPc1[i], '%Y-%m-%d') 
-                      for i in range(len(date_QPc1))])
-
-date_CLc2 = np.array([datetime.strptime(date_CLc2[i], '%Y-%m-%d') 
-                      for i in range(len(date_CLc2))])
-date_HOc2 = np.array([datetime.strptime(date_HOc2[i], '%Y-%m-%d') 
-                      for i in range(len(date_HOc2))])
-date_RBc2 = np.array([datetime.strptime(date_RBc2[i], '%Y-%m-%d') 
-                      for i in range(len(date_RBc2))])
-date_QOc2 = np.array([datetime.strptime(date_QOc2[i], '%Y-%m-%d') 
-                      for i in range(len(date_QOc2))])
-date_QPc2 = np.array([datetime.strptime(date_QPc2[i], '%Y-%m-%d') 
-                      for i in range(len(date_QPc2))])
-
-cumPNL_50_CLc1 = dataframe_CLc1['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_HOc1 = dataframe_HOc1['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_RBc1 = dataframe_RBc1['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_QOc1 = dataframe_QOc1['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_QPc1 = dataframe_QPc1['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-
-cumPNL_50_CLc2 = dataframe_CLc2['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_HOc2 = dataframe_HOc2['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_RBc2 = dataframe_RBc2['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_QOc2 = dataframe_QOc2['cumulative P&L from trades for contracts (x 50)'].to_numpy()
-cumPNL_50_QPc2 = dataframe_QPc2['cumulative P&L from trades for contracts (x 50)'].to_numpy()
+symbol_list = ['CLc1', 'HOc1', 'RBc1', 'QOc1', 'QPc1', 'CLc2', 'HOc2', 'RBc2', 'QOc2', 'QPc2']
+label_list = ['CLc1 (x50)', 'HOc1 (x50)', 'RBc1 (x50)', 'QOc1 (x50)', 'QPc1 (x50)', 'CLc2 (x50)', 'HOc2 (x50)', 'RBc2 (x50)', 'QOc2 (x50)', 'QPc2 (x50)']
+col_list = ['#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6', '#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6']
+line_list = ['-','-', '-','-','-', '--','--', '--','--','--']
 
 def fill_holes(cumPNL):
     if np.isnan(cumPNL[0]):
@@ -95,81 +34,118 @@ def fill_holes(cumPNL):
             cumPNL[i] = cumPNL[i-1]
             
     return cumPNL
+    
+def extract_PNLplot_input(filename, sheet_name='Total', date_col = 'Entry_Date', 
+                          val_col = 'cumulative P&L from trades for contracts (x 50)',
+                          fill_or_not = True):
+          
+    dataframe = pd.read_excel(filename,sheet_name=sheet_name)
 
-cumPNL_50_CLc1 = fill_holes(cumPNL_50_CLc1)
-cumPNL_50_HOc1 = fill_holes(cumPNL_50_HOc1)
-cumPNL_50_RBc1 = fill_holes(cumPNL_50_RBc1)
-cumPNL_50_QOc1 = fill_holes(cumPNL_50_QOc1)
-cumPNL_50_QPc1 = fill_holes(cumPNL_50_QPc1)
+    date_all = dataframe[date_col].to_numpy()
+    cumPNL_all = dataframe[val_col].to_numpy()
+    
+    
+    date_all = np.array([datetime.datetime.strptime(date_all[i], '%Y-%m-%d') 
+                          for i in range(len(date_all))])
 
-cumPNL_50_CLc2 = fill_holes(cumPNL_50_CLc2)
-cumPNL_50_HOc2 = fill_holes(cumPNL_50_HOc2)
-cumPNL_50_RBc2 = fill_holes(cumPNL_50_RBc2)
-cumPNL_50_QOc2 = fill_holes(cumPNL_50_QOc2)
-cumPNL_50_QPc2 = fill_holes(cumPNL_50_QPc2)
+    if fill_or_not:
+        cumPNL_all = fill_holes(cumPNL_all)
+    else:
+        pass
+    
+    return date_all, cumPNL_all
 
-cumPNL_all = fill_holes(cumPNL_all)
 
-# =============================================================================
-# date_list = [date_CLc1, date_HOc1, date_RBc1, date_QOc1, date_QPc1,
-#              date_CLc2, date_HOc2, date_RBc2, date_QOc2, date_QPc2]
-# data_list = [cumPNL_50_CLc1, cumPNL_50_HOc1, cumPNL_50_RBc1, cumPNL_50_QOc1, cumPNL_50_QPc1,
-#              cumPNL_50_CLc2, cumPNL_50_HOc2, cumPNL_50_RBc2, cumPNL_50_QOc2, cumPNL_50_QPc2]
-#  
-# label_list = ['CLc1 (x50)', 'HOc1 (x50)', 'RBc1 (x50)', 'QOc1 (x50)', 'QPc1 (x50)',
-#               'CLc2 (x50)', 'HOc2 (x50)', 'RBc2 (x50)', 'QOc2 (x50)', 'QPc2 (x50)']
-# col_list = ['#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6',
-#             '#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6']
-# 
-# line_list = ['-','-','-','-','-',
-#           '--','--', '--','--','--']
-# 
-# =============================================================================
-
-# =============================================================================
-# date_list = [date_CLc2, date_HOc2, date_RBc2, date_QOc2, date_QPc2]
-# data_list = [cumPNL_50_CLc2, cumPNL_50_HOc2, cumPNL_50_RBc2, cumPNL_50_QOc2, cumPNL_50_QPc2]
-#  
-# label_list = ['CLc2 (x50)', 'HOc2 (x50)', 'RBc2 (x50)', 'QOc2 (x50)', 'QPc2 (x50)']
-# col_list = ['#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6']
-#             
-# 
-# line_list = ['--','--', '--','--','--']
-# 
-# =============================================================================
-
-date_list = [date_CLc1, date_HOc1, date_RBc1, date_QOc1, date_QPc1]
-data_list = [cumPNL_50_CLc1, cumPNL_50_HOc1, cumPNL_50_RBc1, cumPNL_50_QOc1, cumPNL_50_QPc1]
-
-label_list = ['CLc1 (x50)', 'HOc1 (x50)', 'RBc1 (x50)', 'QOc1 (x50)', 'QPc1 (x50)']
-col_list = ['#62A0E1','#EB634E','#E99938','#5CDE93','#6ABBC6']
-line_list = ['-','-', '-','-','-']
-
-def cumPNL_plot(x,y):
+def cumPNL_plot(date, PNL, return_rate=[], line_color = 'w', label = 'All',  
+                 sub_date_list=[], sub_data_list=[], sub_line_list = [],
+                 sub_label_list = [], sub_col_list = []):
+    
     plt.style.use('dark_background')
+    fig = plt.figure(figsize=(14,6))
+    fig.set_tight_layout(True)
+    # make a way to change the number of subplot based on nrows and ncols
+    gs = fig.add_gridspec(nrows=2, ncols = 1, height_ratios=[6, 2.5])
+    fig.suptitle("Cumulative PNL")
+    
+    ax1 = fig.add_subplot(gs[0])
+    ax1.plot(date, PNL,'-', c='w', label='All (x50)')
 
-    # plotting area
+    
+    ax2 = fig.add_subplot(gs[1], sharex=ax1)
 
-    fig, ax = plt.subplots(figsize=(10,4))
+    ax2.bar(date, return_rate, label='All (x50)')
+    ax2.hlines(0, datetime.datetime(2020,12,10), datetime.datetime(2024,7,1), 
+               color='#B67C62', ls='--',lw=4)
     
-    ax.plot(x, y,'-', c='w', label='All (x50)')
+    ax1.set_ylabel('Cumulative return (USD)')
+
     
-    
-    for i, (date, data) in enumerate(zip(date_list, data_list)):
-        ax.plot(date, data,ls=line_list[i], label=label_list[i], color=col_list[i])
+    for i, (date, data) in enumerate(zip(sub_date_list, sub_data_list)):
+        ax1.plot(date, data,ls=sub_line_list[i], 
+                 label=sub_label_list[i], color=sub_col_list[i])
     
     fmt = mdates.DateFormatter('%y-%m-%d')
-    ax.xaxis.set_major_formatter(fmt)
-    #ax.set_yticks(np.arange(0,1e7,2e6), np.arange(0,10_000_000,2_000_000))
+    ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax1.xaxis.set_major_formatter(fmt)
+    ax2.xaxis.set_major_formatter(fmt)
+    
+    ax2.set_ylabel('Scaled returns (USD)')
+    ax2.set_xlabel('Date')
 
-    ax.set_xlabel('Date')
-    ax.set_ylabel('USD')
-    ax.set_title('Cumulative PNL')
-    plt.legend(loc='upper left')
-    plt.grid()
+    ax1.grid()
+    ax2.grid()
     
     plt.show()
-def cumPNL_compare():
-    return
+
+if __name__=='__main__':
     
-cumPNL_plot(date_all, cumPNL_all)
+    
+    # Extract the cumulative PNL of the strategy
+    date_all, cumPNL_all = extract_PNLplot_input(ARGUS_EXACT_PNL_FILENAME)
+    
+    # Extract the trade_return of the strategy
+    date_all2, return_all = extract_PNLplot_input(ARGUS_EXACT_PNL_FILENAME,
+                                                  val_col='scaled returns from trades', 
+                                                  fill_or_not=False)
+    
+    
+    # Extract the individual asset PNL and dates
+    date_list = [extract_PNLplot_input(ARGUS_EXACT_PNL_FILENAME, 
+                                       sheet_name=symbol_list[i], \
+                                           date_col = 'Entry_Date')[0] \
+                                                    for i in range(len(symbol_list))]
+    data_list = [extract_PNLplot_input(ARGUS_EXACT_PNL_FILENAME, 
+                                       sheet_name=symbol_list[i], 
+                                       date_col = 'Entry_Date')[1] \
+                                                     for i in range(len(symbol_list))]
+    return_list = [extract_PNLplot_input(ARGUS_EXACT_PNL_FILENAME, 
+                                         sheet_name=symbol_list[i], 
+                                         val_col='scaled returns from trades', 
+                                         fill_or_not=False)[1] \
+                                                   for i in range(len(symbol_list))]
+    
+    #date_list = [date_CLc1, date_HOc1, date_RBc1, date_QOc1, date_QPc1]
+    #data_list = [cumPNL_50_CLc1, cumPNL_50_HOc1, cumPNL_50_RBc1, cumPNL_50_QOc1, cumPNL_50_QPc1]
+    #cumPNL_plot(date_all, cumPNL_all, label='All (x50)')
+    
+    # =============================================================================
+    # cumPNL_plot(date_all, cumPNL_all, return_all, label='All (x50)',
+    #              sub_date_list=date_list, sub_data_list=data_list,
+    #              sub_label_list = label_list,
+    #              sub_col_list = col_list, 
+    #              sub_line_list =line_list)
+    # =============================================================================
+    # =============================================================================
+    # cumPNL_plot([], [], [], label='All (x50)',
+    #               sub_date_list=date_list, sub_data_list=data_list,
+    #               sub_label_list = label_list,
+    #               sub_col_list = col_list, 
+    #               sub_line_list =line_list)
+    # =============================================================================
+    
+    
+    cumPNL_plot([], [], [], label='All (x50)',
+                  sub_date_list=date_list, sub_data_list=data_list,
+                  sub_label_list = label_list,
+                  sub_col_list = col_list, 
+                  sub_line_list =line_list)
