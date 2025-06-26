cb515ce8bbd4e08c8b609fd4aef7e3ac243e7c62
Dexter <dex-hon-sci@outlook.com>
2025-06-26 14:56:47 +0800

    Adding step2 for vwap strat test, WIP

--- app/run_VWAP_strat_step1.py
+++ app/run_VWAP_strat_step1.py
@@ -64,6 +64,7 @@ def cal_VWAP(df:pd.DataFrame):
     df['VWAP'] = vwap
     df['VWAP_DEV'] = dev
     return df
+
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 
@@ -93,17 +94,17 @@ def plot_VWAP(df, title=''):
     plt.show()
 
 
-def add_VWAP2df(df):
+def add_VWAP2df(df, unique_date):
     # Add VWAP and STD to a dataframe
     # Generate daily VWAP, add it to the dataframe
-    unique_date = list(set([df["Date"].iloc[i] for i,_ in enumerate(df["Date"].to_list())]))
-    unique_date.sort()
+    #unique_date = list(set([df["Date"].iloc[i] for i,_ in enumerate(df["Date"].to_list())]))
+    #unique_date.sort()
 
     new_df = pd.DataFrame()
-    for date in unique_date[1:3]:
+    for date in unique_date[1:2]:
         print(date, type(date))
-        start_date = date
-        end_date = date + datetime.timedelta(hours=23,minutes=59)
+        start_date = date + datetime.timedelta(hours=3,minutes=30)
+        end_date = date + datetime.timedelta(hours=22,minutes=0)
         
         # Select for a sub-dataframe to calculate the vwap of the day
         sub_df = df[(df['Datetime'] >= start_date) &(df['Datetime'] <end_date)]
@@ -112,7 +113,7 @@ def add_VWAP2df(df):
         new_sub_df = cal_VWAP(sub_df)
         
         # Plot the daily chart to check if the VWAP range is reasonable
-        plot_VWAP(new_sub_df,title=f"CLc1: {date.strftime('%m-%d-%Y')}")
+        plot_VWAP(new_sub_df,title=f"CLc1: {date.strftime('%Y-%m-%d')}")
         new_df = pd.concat([new_df, new_sub_df])
     return new_df
         
@@ -123,59 +124,250 @@ def add_VWAP2df(df):
 #myvwap = vwapsum/volumesum
 #dev = sqrt(max(v2sum/volumesum - myvwap*myvwap, 0))
 
+from EC_tools.strategy import Strategy
+from EC_tools.strategy.signal import SignalType, Signal
 
 # New Signal format
-class VWAPInversionStrategy():
-    def __init__():
-        pass
-    
-    def gen_data():
-        return
-    
-    def run_cond(data_15min:pd.DataFrame,
-                 last_data_15min:pd.DataFrame, 
-                 last_data_1min:pd.DataFrame, 
-                 N:float
-                 ):
-        # data_1min: 15 entry datagrame, matching data_15min
-        # data_15min: single entry dataframe 
+class VWAPInversionStrategy(Strategy):
+    def __init__(self, 
+                 aggmin_data:pd.DataFrame, 
+                 N_sigma:float,
+                 TP_multiplier: float|int = 2,
+                 SL_multiplier: float|int = 1,
+                 segment_barmulitplier: float|int = 4):
+        # One active trade at a time
         
-        # Input 15mins data, and 1 mind data
-        # Input VWAP upper and lower Threshold
-        VWAP_up = last_data_15min['VWAP'] + last_data_15min['VWAP_DEV'].iloc[0]*N
-        VWAP_low = last_data_15min['VWAP'] - last_data_15min['VWAP_DEV'].iloc[0]*N
-        
-        # If 15mins data < VWAP upper Threshold
-        # And next 15mins bar close at 70% above the previous bar
-        # ->Sell direction
-        cond_buy_list1= [len(last_data_1min[last_data_1min['Open']>VWAP_up])>0]
-        cond_buy_list2= []
-        
-        # If 15mins data > VWAP upper Threshold
-        # And next 15mins bar close at 70% below the previous bar
-        # ->Sell direction
-        cond_sell_list1= [len(last_data_1min[last_data_1min['Open']<VWAP_low])>0]
-        cond_sell_list2= []
+        # We have to monitor three rows of data
+        # 1) Initial 15 mins for VWAP condition (Signal)
+        # 2) Confirmation Second 15mins for 70% inversion (signal)
+        # 3) Next four 15mins for trading (This one we do it in backtest)
+        super().__init__()
 
-        return 
-    
-    def set_EES():
+        self._aggmin_data = aggmin_data #aggegrated minute data (Any mins chuck)
+        self._N_sigma = N_sigma # mulitplying factor for sigma
+        self._TP_multiplier = TP_multiplier
+        self._SL_multiplier = SL_multiplier
+        self._segment_barmulitplier = segment_barmulitplier
         
-        entry_price = 0
+        # Calculate the ranges for the VWAP Inversion
+        self.VWAP_up = self._aggmin_data['VWAP'] + self._aggmin_data['VWAP_DEV']*self._N_sigma
+        self.VWAP_low = self._aggmin_data['VWAP'] - self._aggmin_data['VWAP_DEV']*self._N_sigma
+            
+        self._sub_buy_cond_dict = dict()
+        self._sub_sell_cond_dict = dict()
+        self.sub_cond_dict = {'Buy':[], 'Sell':[], 'Neutral': []}
         
-        return 
+        self.strategy_name = 'VWAP_Inversion'
+
     
-    def apply_strategy():
-        return
+    def gen_data(self) -> tuple[np.array]:
+        # Find the time(index) where one of the 15min OHLC is higher/lower
+        # than the threshold
+        # The boolean value for price going above the upper VWAP threshold
+        #up_bool_open = np.sign(self._aggmin_data['Open'] - self.VWAP_up) > 0
+        up_bool_high = np.sign(self._aggmin_data['High'] - self.VWAP_up) > 0
+        up_bool_low = np.sign(self._aggmin_data['Low'] - self.VWAP_up) > 0
+        #up_bool_close = np.sign(self._aggmin_data['Settle'] - self.VWAP_up) > 0
+        
+        # If any of these are true, it means 
+        #up_bool = (up_bool_open or up_bool_high or up_bool_low or up_bool_close)
+        up_bool = [(bool1 or bool2) for bool1,bool2 in zip(up_bool_high, up_bool_low)]
+        
+        # The boolean value for price going below the lower VWAP threshold
+        #low_bool_open = np.sign(self._aggmin_data['Open'] - self.VWAP_low) < 0
+        low_bool_high = np.sign(self._aggmin_data['High'] - self.VWAP_low) < 0 
+        low_bool_low = np.sign(self._aggmin_data['Low'] - self.VWAP_low) < 0
+        #low_bool_close = np.sign(self._aggmin_data['Settle'] - self.VWAP_low) <0
+        
+        #print("up_bool_high", up_bool_high, "up_bool_low", up_bool_low)
+        #print("low_bool_high", low_bool_high,"low_bool_low", low_bool_low)
+        
+        # If any of these are true, it means the bar has hit the threshold in this minute
+        # XOR condition, if 
+        #low_bool = (low_bool_open or low_bool_high or low_bool_low or low_bool_close)
+        low_bool = [(bool1 or bool2) for bool1,bool2 in zip(low_bool_high, low_bool_low)]
+        #print("up_bool", up_bool)
+        #print("low_bool", low_bool)
+
+        upbreach_indices = np.arange(len(self._aggmin_data))[up_bool]
+        downbreach_indices = np.arange(len(self._aggmin_data))[low_bool]
+        #print("upbreach_indices", upbreach_indices)
+        #print("downbreach_indices", downbreach_indices)
+        return upbreach_indices, downbreach_indices
+    
+    def run_cond(self, 
+                 upbreach_indices:np.array, 
+                 downbreach_indices: np.array,
+                 barclose_threshold_factor: float = 0.75) -> list[dict]:
+        # Loop through the day based on the upbreach_indices and downbreach_indices
+
+        for upbreach_index in upbreach_indices:
+            # If 15mins data < VWAP upper Threshold
+            # And next 15mins bar close at 70% above the previous bar
+            # -> Sell direction
+            #cond_buy_list1= [len(last_data_1min[last_data_1min['Open']>VWAP_up])>0]
+            bar_width = self._aggmin_data['High'].iloc[upbreach_index] - \
+                        self._aggmin_data['Low'].iloc[upbreach_index] 
+            barclose_threshold = self._aggmin_data['High'].iloc[upbreach_index] - \
+                                 bar_width* barclose_threshold_factor
+            
+            second_settle = self._aggmin_data['Settle'].iloc[upbreach_index+1]
+            # IF the close of the next bar is lower than the limit
+            cond_sell_list1= [(second_settle < barclose_threshold)]
+            
+            # Assign direction
+            direction = SignalType.SELL if cond_sell_list1[0] else SignalType.NEUTRAL
+            
+            if direction == SignalType.SELL:
+                
+                #for Sell signal, next 2 entry Sell
+                # Target Entry time and price 
+                TE_time = self._aggmin_data['Datetime'].iloc[upbreach_index+2] 
+                TE_price = self._aggmin_data['Open'].iloc[upbreach_index+2]
+                # Target Entry time and price 
+                TP_price = self._aggmin_data['Open'].iloc[upbreach_index+2] - \
+                          bar_width*self._TP_multiplier
+                # Stop-Loss time and price 
+                SL_price = self._aggmin_data['Open'].iloc[upbreach_index+2] + \
+                           bar_width*self._SL_multiplier
+                # Close Order time
+                CO_time = self._aggmin_data['Datetime'].iloc[upbreach_index+2+self._segment_barmulitplier]
+                
+                signal_dict = {"direction": direction, 
+                               'TE_ordertype': "MKT", # for MKT, entry search for time, for LMT or STP, search for price
+                               "TE_time": TE_time,
+                               "TE_price": TE_price,
+                               "segment_starttime": self._aggmin_data['Datetime'].iloc[upbreach_index+2],
+                               "segment_endtime": self._aggmin_data['Datetime'].iloc[upbreach_index+2+self._segment_barmulitplier],
+                               "TP_ordertype": "LMT",
+                               "TP_price": TP_price,
+                               "TP_ordertype": "STP",
+                               "SL_price": SL_price
+                               }
+                print(signal_dict)
+                
+                
+                Signal()
+                
+                action1 = {"order_side": direction,
+                           "order_type": "MKT",
+                           "time": TE_time,
+                           "price": TE_price, 
+                           }
+                
+                action2 = {"order_side": SignalType.BUY,
+                           "order_type": "LMT",
+                           "price": TP_price, 
+                           }
+                
+                action3 = {"order_side": SignalType.BUY,
+                           "order_type": "STP",
+                           "price": SL_price, 
+                           }
+                action4 = {"order_side": SignalType.BUY,
+                           "order_type": "STP",
+                           "price": SL_price}
+                
+        for downbreach_index in downbreach_indices:
+            # If 15mins data > VWAP upper Threshold
+            # And next 15mins bar close at 70% below the previous bar
+            # ->Sell direction
+            #cond_sell_list1= [len(last_data_1min[last_data_1min['Open']<VWAP_low])>0]
+            bar_width = self._aggmin_data['High'].iloc[downbreach_index] - \
+                        self._aggmin_data['Low'].iloc[downbreach_index] 
+            barclose_threshold = self._aggmin_data['Low'].iloc[downbreach_index] + \
+                                 bar_width* barclose_threshold_factor
+                                 
+            second_settle = self._aggmin_data['Settle'].iloc[downbreach_index+1]
+
+            # IF the close of the next bar is higher than the limit
+            cond_buy_list1= [second_settle > barclose_threshold]
+            
+            # Assign direction
+            direction = SignalType.BUY if cond_sell_list1[0] else SignalType.NEUTRAL
+            
+            
+            if direction == SignalType.BUY:
+                pass
+
+         
+    
+    def set_EES(self):
+        # Duration (default = 4 bars)
+        # set EES prices
+        match self.direction:
+            case SignalType.BUY:
+                # (A) Entry price
+                entry_price = float(0)
+                # (B) Exit price
+                exit_price = float(0)
+                # (C) Stop loss at APC p=0.1
+                stop_loss = float(0)
+                print("Buy Direction!")
+
+                
+            case SignalType.SELL:
+                # (A) Entry price
+                entry_price = float(0)
+                # (B) Exit price
+                exit_price = float(1)
+                # (C) Stop loss at APC p=0.9
+                stop_loss = float(2)
+                print("Sell Direction!")
+
+            
+            case SignalType.NEUTRAL:
+                entry_price = "NA"
+                exit_price = "NA"
+                stop_loss = "NA"
+                print("Neutral Direction!")
+            case _:
+                raise Exception(
+                'Unaccepted input, condition needs to be either Buy, \
+                    Sell, or Neutral.')
+            
+        return entry_price, exit_price, stop_loss
+    
+    def apply_strategy(self):
+        
+        # generate a pair of upbreach and downbreach indices
+        # This shorten the loop by vectorising the intraday data.
+        upbreach_indices, downbreach_indices = self.gen_data()
+        print("up", upbreach_indices, len(upbreach_indices),
+              "down", downbreach_indices, len(downbreach_indices))
+        #
+        #direction, cond_info = self.run_cond(upbreach_indices, downbreach_indices)
+        self.run_cond(upbreach_indices, downbreach_indices)
+        
+        #entry_price, exit_price, stop_loss = self.set_EES()
+
+
+        # Bookkeeping area
+        #EES = [entry_price, exit_price, stop_loss]
+        #EES_val = [entry_price, exit_price, stop_loss]
+        
+        
+        # put all the data in a singular list. This is to be added in the 
+        # data list in the loop
+        
+        #data =  cond_info + quantile_info + EES_val + [self.strategy_name]
+        
+        return #{'data': data, 'direction': direction.value}
+
     
 # Loop daily and find
 def loop_signal():
-    
+    for i in range(10):
+        
+        VWAPInversionStrategy(new_df,1.2).apply_strategy()
+
+        pass
     
     return 
 
 
 def main():
+    
     # Load Historical data
     #HISTORY_MINUTE_PKL = load_source_data_bt(list(DAILY_MINUTE_DATA_INDI_PKL.values()))
     HISTORY_MINUTE_PKL = load_source_data_bt([DAILY_MINUTE_DATA_INDI_PKL['CLc1']])
@@ -191,13 +383,20 @@ def main():
     # Select for only entries after 2021-1-1
     new_df = new_df[new_df['Datetime'] > datetime.datetime(2024,10,4)]
     print(new_df)
+    
+    # Define unique trading date for the script to loop through
+    unique_date = list(set([new_df["Date"].iloc[i] 
+                       for i,_ in enumerate(new_df["Date"].to_list())]))
+    unique_date.sort()
+    
     # Calculate VWAP, save it in the dataframe as a new column.
     # Plot them if needed
-    new_df = add_VWAP2df(new_df)
+    new_df = add_VWAP2df(new_df, unique_date)
     
-    print("new_df", new_df)
+    print("new_df", new_df, len(new_df))
     
     # New Signal format
     # Loop daily and find
+    VWAPInversionStrategy(new_df,1.2).apply_strategy()
 
 main()
---
+++ app/run_VWAP_strat_step2.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 26 13:06:00 2025
+
+@author: dexter
+"""
+
+# New Backtest
+# Save in object format, make function to redner it to 
+# Signal_generation: def: use strategy, generate signals 
+# Signal contains precise execution instructions (MKT: time, LMT:price, Dynamic) 
+# Backtest use Trade method to calculate the correct entry/exit point (class attr)
+# Trade method also add the correct exeuciton record to the Portfolio object
+
+# input config, Define data (Signal list, historical price) input 
+#####
+# Backtest type
+
+# Independent: process backtest by signals in a list intead of time
+# Concurrent: process backtest by time instead of signal
+
+# LMT order calculation method (RANGE VS CROSSOVER)
+# OneActive Signal vs MultActive Signals per asset
+#########
+# Define Backtest, 
+# loop start, define time increment (by signal or by fixed time)
+# Look for Signal Segementation (isolate segment to calculate trade return)
+
+# Indepednet:
+# 0) Find active signal in a day (basetime setting)
+# 1) Group signals by start_time
+# 2) Extract intraday_minute_data (based on the ACTIVE signal of that asset)
+# 3) Use Trade method, calculate correct entry/exit points
+# 3.1) For Dynamic SL, trade caclculation is predefined segment loop.
+# 4) Use the exit_point of Trade method, check if the signal in siganl_list comes after it
+# If not, pop it. (garbage_signals)
+
+# Concurrent
+# Group all assets signals and price data by date (or any time interval)
+# Process all signals simultaneously
+# Run crossover point check, align all cross-over points of all asset in
+# one sequence sorted by time.
+# Process each cross-over point one-by-one and after each step, calculate if 
+# there is any factor changes if the strategy relies on aggegrated feedbacks
+import pandas as pd
+from EC_tools.portfolio import Portfolio
+
+def backtest_engine(portfo: Portfolio,
+                    signals: dict[str, pd.DataFrame],
+                    histroy: dict[str, pd.DataFrame], 
+                    **kwargs):
+    # The main loop for backtesting
+    return
+# =============================================================================
+# def loop_portfolio_preloaded_dSL(portfo: Portfolio, 
+#                                  trade_method,
+#                                  signal_table: pd.DataFrame, 
+#                                  histroy_intraday_data_pkl: dict[str, pd.DataFrame], 
+#                                  **kwargs):
+#     # Loop through signal master table and execute trade in the intraday data
+#     # Within this custom loop, there is another loop that go through a series of
+#     # sections and calculate entry point and exit point individually.
+#     default_kwargs = DEFAULT_KWARGS
+#     kwargs = dict(default_kwargs,**kwargs)
+# 
+#     for i in range(len(signal_table)):
+# 
+#         # setup trade inputs ###########
+#         item = signal_table.iloc[i]
+#                 
+#         symbol = item['Price_Code']
+#         date_interest = item['Date']
+#         get_obj_name = item['Price_Code']
+# 
+#         open_hr = kwargs['open_hr_dict'][symbol]
+#         close_hr = kwargs['close_hr_dict'][symbol]
+#         
+#         histroy_intraday_data = histroy_intraday_data_pkl[symbol]
+#         
+#         day = backtest.extract_intraday_minute_data(histroy_intraday_data, 
+#                                                     date_interest, 
+#                                                     open_hr=open_hr, 
+#                                                     close_hr=close_hr)
+#         
+#         open_hr_dt, open_price = read.find_closest_price(day,
+#                                                          target_hr= open_hr,
+#                                                          direction='forward')
+#         
+#         close_hr_dt, close_price = read.find_closest_price(day,
+#                                                            target_hr= close_hr,
+#                                                            direction='backward')
+#             
+#         # The time to open all positions
+#         pos_open_dt = datetime.datetime.combine(date_interest.date(), open_hr_dt)
+#         
+#         print('===============================')
+#         print(i, pos_open_dt, symbol)
+#         #print('day', day)
+#         #print('Time', day['Time'].iloc[0], type(day['Time'].iloc[0]))
+#         # Target EES global (the initial targets)
+#         global_target_entry = item['Entry_Price']
+#         global_target_exit = item['Exit_Price']
+#         global_stop_exit = item['StopLoss_Price']
+# 
+#         # Build up sections, the output is a dict containing the EES for each 
+#         # section
+#         sections = load_EES_from_signal_dynamic(item)
+#         #print('sections', sections)
+#         # Setup trade ##########
+#         trade_id = i #direction + str(i)
+#         direction = sections['0']['direction'][1]
+#         # Make a list of SL for universal access
+#         dyn_list = item[['StopLoss_Price_1', 'StopLoss_Price_2', 
+#                          'StopLoss_Price_3','StopLoss_Price_4']].to_list()
+# 
+#         # Initialise trade 
+#         T = trade_method(portfo, trade_id, 
+#                          trail_price_delta=kwargs['trail_price_delta'], 
+#                          direction=direction,
+#                          cross_decision=kwargs['cross_decision'],
+#                          dyn_list = dyn_list)
+#         
+#         # Setup the close hour exit point (remember to change the close_hr_dt 
+#         # from time to datetime)
+#         T._close_pt = (datetime.datetime.combine(date_interest, close_hr_dt), close_price)
+#         
+#         # Loop Through each section to find the suitable EES point, 
+#         # Store them in the class variable in the Trade object
+#         for num in sections:
+#             # Isolate day_section from day data according to the time range
+#             # of the EES
+#             start_time = datetime.datetime.strptime(sections[num]['stop_exit'][0][0], 
+#                                                     '%H:%M:%S').time()
+#             end_time = datetime.datetime.strptime(sections[num]['stop_exit'][0][1], 
+#                                                   '%H:%M:%S').time()
+#             
+#             day_section = day[(day['Time']>=start_time) & (day['Time']<=end_time)]
+#             #print('day_section', day_section)
+#             
+#             if len(day_section) == 0:
+#                 print('day_section is empty!')
+#                 break
+#             
+#             # The EES for this section
+#             target_entry = sections[num]['target_entry'][1]
+#             target_exit = sections[num]['target_exit'][1]
+#             stop_exit = sections[num]['stop_exit'][1]
+#             
+#             print_SL_price = T._dyn_list[int(num)]
+#             
+#             print(f'--------section {num}: {start_time} to {end_time}, "{direction}"--------')
+#             print(f'TE: {target_entry}, TP: {target_exit}, SL: {print_SL_price}')
+#             print('------------------------------------------------------')
+# 
+#             # set the open_hr to the time specific to this section
+#             open_hr_dt, close_hr_dt = start_time, end_time
+# 
+#             # Generate truncation dictionary in this section of the day
+#             trunc_dict, target_entry, \
+#             target_exit, stop_exit = backtest.gen_trunc_dict(LoopType.CROSSOVER,
+#                                                              day_section, 
+#                                                              target_entry, 
+#                                                              target_exit, 
+#                                                              print_SL_price, 
+#                                                              open_hr_dt, 
+#                                                              close_hr_dt, 
+#                                                              direction)
+#             #print(trunc_dict)
+#             
+#             
+#             # Choose the earliest EES for this section
+#             T.choose_EES_values(trunc_dict, dyn_list, int(num),
+#                                 trail_price_delta = TRAIL_PRICE_DELTA[symbol],
+#                                 direction=direction)
+#             print('------------------------------------------------------')
+#         print('------------------------------------------------------')
+# 
+#         # Export the trade object defacto EES points
+#         # A list of initial target prices for opening position, it will be 
+#         # changed during execute_order based on EES_pt_list
+#         EES_target_price_list = [global_target_entry, global_target_exit, 
+#                                  global_stop_exit, close_price]
+#         
+#         # A list of final EES points
+#         EES_pt_list = [T._TE_pt, T._TP_pt, T._SL_pt, T._close_pt]
+#         
+#         ORDER_TYPE = {'Buy': 'Long', 'Sell':'Short'}
+#         #print('EES_target_list', EES_target_price_list)
+#         print('EES_pt_list', EES_pt_list)
+#         
+#         # Open position and add them in the portfolio
+#         pos_list = T.open_positions(kwargs['give_obj_name'], 
+#                                     get_obj_name, 
+#                                     kwargs['get_obj_quantity'],
+#                                     EES_target_price_list, 
+#                                     ORDER_TYPE[direction],
+#                                     size=SIZE_DICT[get_obj_name],
+#                                     fee=OIL_FUTURES_FEE, 
+#                                     open_time = open_hr_dt)
+#         #print('pos_list', pos_list)
+#         print('------------------------------------------------------')
+# 
+#         # Execute and exit position in the portfolio
+#         trade_open, trade_close, \
+#         pos_list, exec_pos_list = T.execute_positions(EES_pt_list, 
+#                                                       pos_list, 
+#                                                       order_type = \
+#                                                       ORDER_TYPE[direction])
+#         
+#         #print('pos_list', pos_list)
+#         #print(exec_pos_list)
+#         print('------------------------------------------------------')
+# 
+#         #backtest.plot_in_backtest(date_interest,get_obj_name, trunc_dict, direction, 
+#         #                          plot_or_not=kwargs['plot_or_not'])
+#                                                  
+# 
+#     return portfo
+# =============================================================================
